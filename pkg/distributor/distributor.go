031b55aacb278b4d35bd62ec7eaa1f1877894b99
Goutham Veeramachaneni <gouthamve@gmail.com>
2021-04-16 08:50:39 +0200

    Also add a flag for debug series.
    Signed-off-by: Goutham Veeramachaneni <gouthamve@gmail.com>

--- pkg/distributor/distributor.go
+++ pkg/distributor/distributor.go
@@ -31,6 +31,7 @@ import (
 	"github.com/cortexproject/cortex/pkg/tenant"
 	"github.com/cortexproject/cortex/pkg/util"
 	"github.com/cortexproject/cortex/pkg/util/extract"
+	"github.com/cortexproject/cortex/pkg/util/flagext"
 	"github.com/cortexproject/cortex/pkg/util/limiter"
 	"github.com/cortexproject/cortex/pkg/util/math"
 	"github.com/cortexproject/cortex/pkg/util/services"
@@ -142,6 +143,7 @@ func (cfg *Config) RegisterFlags(f *flag.FlagSet) {
 	f.BoolVar(&cfg.ShardByAllLabels, "distributor.shard-by-all-labels", false, "Distribute samples based on all labels, as opposed to solely by user and metric name.")
 	f.StringVar(&cfg.ShardingStrategy, "distributor.sharding-strategy", util.ShardingStrategyDefault, fmt.Sprintf("The sharding strategy to use. Supported values are: %s.", strings.Join(supportedShardingStrategies, ", ")))
 	f.BoolVar(&cfg.ExtendWrites, "distributor.extend-writes", true, "Try writing to an additional ingester in the presence of an ingester not in the ACTIVE state. It is useful to disable this along with -ingester.unregister-on-shutdown=false in order to not spread samples to extra ingesters during rolling restarts with consistent naming.")
+	f.Var((*flagext.StringSlice)(&cfg.DebugSeries), "distributor.debug-series", "The series to print the timestamps and values for.")
 }
 
 // Validate config and returns error on failure
